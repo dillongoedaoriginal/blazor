2307ff8f939d4ce3992a064d17e1f274750803f8
Dillon Goeda <dillong@dpdlaser.co.za>
2019-08-14 21:38:41 +0200

    Initial Commit

---
+++ RaspberryPiDashboard/Manager/AzureStorageQueue.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public class AzureStorageQueue : IEnqueueable<string>
+    {
+        private readonly string queueName;
+        private readonly string connectionString;
+
+
+        public AzureStorageQueue(string queueName, string connectionString)
+        {
+            this.queueName = queueName;
+            this.connectionString = connectionString;
+        }
+
+
+        public async Task<bool> EnqueueAsync(string message)
+        {
+            var queue = CreateQueueAsync(queueName, connectionString);
+            await queue.AddMessageAsync(new CloudQueueMessage(message, true));
+            return true;
+        }
+
+
+        private CloudQueue CreateQueueAsync(string queueName, string connectionString)
+        {
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            var queueClient = storageAccount.CreateCloudQueueClient();
+
+            var queue = queueClient.GetQueueReference(queueName);
+
+
+            return queue;
+        }
+    }
+}
